47fb34441b0fdbef4c660454be9e78d7e6265f2d
Alireza Naimi, PhD, PE <alienaimi@gmail.com>
2020-09-25 16:07:56 -0400

    Update cube_net2shp.bat

--- cube_net2shp.bat
+++ cube_net2shp.bat
@@ -1,15 +1,15 @@
 /*
 @call "%~dp0cube_p" %*&&@TITLE %~n0 (%*)&@echo -- %~n0 (%*)
 
-@set al_o_link=%~dp1%~n1_link
-@set al_o_node=%~dp1%~n1_node
+@set al_o_link=%~dpn1_link
+@set al_o_node=%~dpn1_node
 
 @set crs=PROJCS["NAD_1983_2011_StatePlane_Maryland_FIPS_1900_Ft_US",GEOGCS["GCS_NAD_1983_2011",DATUM["D_NAD_1983_2011",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Lambert_Conformal_Conic"],PARAMETER["False_Easting",1312333.333333333],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",-77.0],PARAMETER["Standard_Parallel_1",38.3],PARAMETER["Standard_Parallel_2",39.45],PARAMETER["Latitude_Of_Origin",37.66666666666666],UNIT["Foot_US",0.3048006096012192]]
 @rem @set crs=PROJCS["NAD_1983_StatePlane_Virginia_North_FIPS_4501_Feet",GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137,298.257222101]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],PROJECTION["Lambert_Conformal_Conic"],PARAMETER["False_Easting",11482916.66666666],PARAMETER["False_Northing",6561666.666666666],PARAMETER["Central_Meridian",-78.5],PARAMETER["Standard_Parallel_1",38.03333333333333],PARAMETER["Standard_Parallel_2",39.2],PARAMETER["Latitude_Of_Origin",37.66666666666666],UNIT["Foot_US",0.30480060960121924]]
 @echo %crs%>"%al_o_link%.prj" &type "%al_o_link%.prj" > "%al_o_node%.prj"
 
-VOYAGER "%~dp0%~nx0" %args%
-@rem TPPLUS "%~dp0%~nx0" %args%
+VOYAGER "%~dpnx0" %args%
+@rem TPPLUS "%~dpnx0" %args%
 
 @timeout 10 &goto:eof&::██████████████████████████████████████████████████████*/
 
