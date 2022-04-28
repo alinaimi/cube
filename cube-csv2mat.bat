908c77e2d115189eaa2c115af1362582eea51c21
Alireza Naimi, PhD, PE <alienaimi@gmail.com>
2022-04-28 09:32:41 -0400

    Update cube-csv2mat.bat

--- cube-csv2mat.bat
+++ cube-csv2mat.bat
@@ -2,17 +2,18 @@
 set @=%~f0&call "%~dp0cube-p" %*
 
 set zones=3722
-VOYAGER "%~dpnx0" %args%
 
-@%SystemRoot%\System32\timeout 10 &goto:eof&::------------------------------------------------------------------------*/
+VOYAGER "%~f0" %args%
 
+@call "%~dp0cube-q" %* &goto:eof&::--------------------------------------------------------------------*/%add2script%
 sum=0
+
 
 // vector to mat
 Run pgm=MATRIX PRNFILE="%PRNFILE%"
-  FILEI MATI[1] = "%dpnx1%", PATTERN=IJ:V FIELDS=#1,2,3
-  //FILEI MATI[1] = "%dpnx1%", PATTERN=IJ:V, FIELDS=#1,0,2-4, SKIPRECS =1
-  FILEO MATO[1]="%dpn1%-%@%.mat", mo=1, DEC=2 ;// , MO=1 NAME=TRIPS , mo=1, DEC=2
+  FILEI MATI[1] = "%f1%", PATTERN=IJ:V FIELDS=#1,2,3
+  //FILEI MATI[1] = "%f1%", PATTERN=IJ:V, FIELDS=#1,0,2-4, SKIPRECS =1
+  FILEO MATO[1]="%dpn1%-%@_nam_%.mat", mo=1, DEC=2 ;// , MO=1 NAME=TRIPS , mo=1, DEC=2
   PAR  ZONES=%zones%
   ZONES=%zones%
 
@@ -22,8 +23,8 @@ EndRun
 
 // matrix to mat
 Run pgm=MATRIX MSG='Convert CSV in matrix format into .MAT'
-  FILEI MATI[1] = "%dpnx1%", PATTERN=IJ:V, FIELDS=#1,0,2-4, SKIPRECS =1
-  FILEO MATO[1] = "%dpn1%-%@%-2d.mat", MO=1
+  FILEI MATI[1] = "%f1%", PATTERN=IJ:V, FIELDS=#1,0,2-4, SKIPRECS =1
+  FILEO MATO[1] = "%dpn1%-%@_nam_%-2d.mat", MO=1
 
   ZONES=%zones%
   MW[1]=MI.1.1
